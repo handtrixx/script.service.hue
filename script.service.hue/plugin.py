13d1167c18958a50561cec5374353191a64ed075
handtrixxx <pooh85@gmx.de>
2022-02-20 13:43:48 +0100

    removed orig directoy to upload customized one

--- script.service.hue/plugin.py
+++
@@ -1,9 +0,0 @@
-import xbmc
-
-from resources.lib import menu, reporting
-
-try:
-    menu.menu()
-except Exception as exc:
-    xbmc.log(f"[script.service.hue][EXCEPTION] Plugin exception: {exc}")
-    reporting.process_exception(exc)
--- script.service.hue/service.py
+++
@@ -1,9 +0,0 @@
-import xbmc
-
-from resources.lib import core, reporting
-
-try:
-    core.core()
-except Exception as exc:
-    xbmc.log(f"[script.service.hue][EXCEPTION] Service exception: {exc}")
-    reporting.process_exception(exc)
